87f83563ca552a3b7eeaac9a3b172f31f12b6771
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-10-04 18:43:29 -0700

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
@@ -12,6 +13,6 @@ export class ProductService {
   }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map(response => response.json());
+    return this._http.get(this._albumUrl).map(response => <Album>response.json() );
   }
 }
